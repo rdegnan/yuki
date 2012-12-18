479070f8632419b4278d449e2b462ab1d66ac032
Ryland Degnan <ryland.degnan@mrnumber.com>
2012-12-18 13:03:27 -0800

    Move imperative implementations to submodule

--- src/yuki.mli
+++ src/yuki.mli
@@ -8,26 +8,7 @@ module RandomAccessList(Conn:Make.Conn)(Elem:Make.Elem) : sig
   (* head and tail raise Empty if list is empty *)
 
   val lookup : string -> int -> Elem.t Lwt.t
-  val update : string -> int -> Elem.t -> string Lwt.t
-  (* lookup and update raise Subscript if index is out of bounds *)
-
-  val page : string -> int -> int -> (Elem.t list * bool) Lwt.t
-  val take_while : string -> (Elem.t -> bool) -> Elem.t list Lwt.t
-
-  val fold_left : string -> ('a -> Elem.t -> 'a Lwt.t) -> 'a -> 'a Lwt.t
-  val fold_right : string -> (Elem.t -> 'a -> 'a Lwt.t) -> 'a -> 'a Lwt.t
-end
-
-module RandomAccessListNoExn(Conn:Make.Conn)(Elem:Make.Elem) : sig
-  val size : string -> int Lwt.t
-
-  val cons : string -> ?key:string -> Elem.t -> unit Lwt.t
-  val head : string -> Elem.t Lwt.t
-  val pop : string -> Elem.t Lwt.t
-
-  val lookup : string -> int -> Elem.t Lwt.t
-  val update : string -> int -> Elem.t -> unit Lwt.t
-    (* lookup and update raise Subscript if index is out of bounds *)
+  (* lookup raises Subscript if index is out of bounds *)
 
   val page : string -> int -> int -> (Elem.t list * bool) Lwt.t
   val take_while : string -> (Elem.t -> bool) -> Elem.t list Lwt.t
@@ -44,4 +25,30 @@ module Heap(Conn:Make.Conn)(Elem:Make.Ord) : sig
   val find_min : string -> Elem.t Lwt.t
   val delete_min : string -> (Elem.t * string) Lwt.t
   (* find_min and delete_min raise Empty if heap is empty *)
+end
+
+module Imperative : sig
+  module RandomAccessList(Conn:Make.Conn)(Elem:Make.Elem) : sig
+    val size : string -> int Lwt.t
+
+    val cons : string -> ?key:string -> Elem.t -> unit Lwt.t
+    val head : string -> Elem.t Lwt.t
+    val pop : string -> Elem.t Lwt.t
+
+    val lookup : string -> int -> Elem.t Lwt.t
+    (* lookup raises Subscript if index is out of bounds *)
+
+    val page : string -> int -> int -> (Elem.t list * bool) Lwt.t
+    val take_while : string -> (Elem.t -> bool) -> Elem.t list Lwt.t
+
+    val fold_left : string -> ('a -> Elem.t -> 'a Lwt.t) -> 'a -> 'a Lwt.t
+    val fold_right : string -> (Elem.t -> 'a -> 'a Lwt.t) -> 'a -> 'a Lwt.t
+  end
+
+  module Heap(Conn:Make.Conn)(Elem:Make.Ord) : sig
+    val insert : string -> Elem.t -> unit Lwt.t
+
+    val find_min : string -> Elem.t Lwt.t
+    val delete_min : string -> Elem.t Lwt.t
+  end
 end
