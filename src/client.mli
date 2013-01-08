fa5735f96e935a56942148a5a07ad7fd555411bb
Matthew Tovbin <tovbinm@gmail.com>
2013-01-08 12:55:37 -0800

    Use riak_put instead of riak_put_raw

--- src/client.mli
+++ src/client.mli
@@ -4,12 +4,11 @@ module Make(Conn:Make.Conn)(Elem:Make.Elem) : sig
   type t = {
     key : riak_key;
     value : Elem.t;
-    vclock : riak_vclock;
     links : riak_key list;
   }
 
   val get : riak_key -> t Lwt.t
-  val put : ?key:riak_key -> ?v:riak_vclock -> ?ops:riak_put_option list -> Elem.t -> riak_key list -> t Lwt.t
+  val put : ?key:riak_key -> ?ops:riak_put_option list -> Elem.t -> riak_key list -> t Lwt.t
 
   val read : riak_key -> (Elem.t -> 'a Lwt.t) -> 'a Lwt.t
   val read_default : riak_key -> Elem.t -> (Elem.t -> 'a Lwt.t) -> 'a Lwt.t
